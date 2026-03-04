e9a2ffaaf00e373f22674f2e4d3c8afa84a2c9d7
alextry004 <alextry004@gmail.com>
2026-03-04 14:32:57 +0100

    added 5 different temperatures alongside an indication bar

--- lattice_gen.py
+++ lattice_gen.py
@@ -10,25 +10,45 @@ def spin_average(spin_2d):
     return total / n
 
 gridsize = 100
-temperature = 1.5
+T_values = [1.0, 1.5, 2.0, 2.5, 3.0]
+T_values = [abs(T) for T in T_values]
+MC_steps = 100
 
-lattice_n = Lattice(gridsize, temperature)
 
-MC_steps = 100  
-m_hist = []
+for temperature in T_values:
+    lattice_n = Lattice(gridsize, temperature)
+    m_hist = []
 
-for step in range(MC_steps):
-    for _ in range(gridsize**2):
-        lattice_n.step()
-    m_hist.append(spin_average(lattice_n.spin))
+    for step in range(MC_steps):
+        for _ in range(gridsize**2):
+            lattice_n.step()
+        m_hist.append(spin_average(lattice_n.spin))
 
-plt.figure()
-plt.plot(m_hist)
+    plt.plot(m_hist, label=f"T={temperature}")
+
 plt.xlabel("MC step")
 plt.ylabel("Spin average  m = <s>")
+plt.legend()
 plt.show()
 
-for _ in range(MC_steps*gridsize**2):
-    lattice_n.step()
-plt.imshow(lattice_n.spin, cmap="gray")
+
+fig, axes = plt.subplots(1, len(T_values), figsize=(8, 5))
+
+for ax, T in zip(axes, T_values):
+    lattice_n = Lattice(gridsize, T)
+
+    for i in range(MC_steps):
+        print(f"\rGenerating lattice series... MC step {i+1}/{MC_steps}", end="", flush=True)
+
+        for _ in range(gridsize**2):
+            lattice_n.step()
+    print()
+    ax.imshow(lattice_n.spin, cmap="gray")
+    ax.axis("off")
+    ax.set_title(f"T = {T}")
+    
+    
+
+plt.tight_layout()
 plt.show()
+
