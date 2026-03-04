0a5144ead2b1c546af183ab93ff03bb92f0dbbc6
alextry004 <alextry004@gmail.com>
2026-03-04 13:33:49 +0100

    Spin average plot

--- lattice_gen.py
+++ lattice_gen.py
@@ -1,11 +1,34 @@
-from lattice import Lattice
 import matplotlib.pyplot as plt
+from lattice import Lattice
+
+def spin_average(spin_2d):
+    total = 0
+    n = 0
+    for row in spin_2d:
+        total += sum(row)
+        n += len(row)
+    return total / n
 
 gridsize = 100
-temperature = 0.5
-lattice = Lattice(gridsize, temperature)
-MC_steps = 100
+temperature = 1.5
+
+lattice_n = Lattice(gridsize, temperature)
+
+MC_steps = 100  
+m_hist = []
+
+for step in range(MC_steps):
+    for _ in range(gridsize**2):
+        lattice_n.step()
+    m_hist.append(spin_average(lattice_n.spin))
+
+plt.figure()
+plt.plot(m_hist)
+plt.xlabel("MC step")
+plt.ylabel("Spin average  m = <s>")
+plt.show()
+
 for _ in range(MC_steps*gridsize**2):
-    lattice.step()
-plt.imshow(lattice.spin, cmap="gray")
+    lattice_n.step()
+plt.imshow(lattice_n.spin, cmap="gray")
 plt.show()
