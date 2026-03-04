494c1cb8a5d517800c839cba75d7bd229c187fab
OlavKvalbein <olav.kvalbein03@gmail.com>
2026-03-04 13:17:10 +0100

    changed to numpy implementation

--- lattice.py
+++ lattice.py
@@ -1,39 +1,47 @@
 import matplotlib.pyplot as plt
-import math
-import random
+import numpy as np
 
 class Lattice():
     def __init__(self, gridsize, temperature):
         self.size = gridsize
         self.T = temperature
-        self.spin = [[random.choice([-1,1]) for _ in range(self.size)] for _ in range(self.size)]
+        self.rng = np.random.default_rng()
+        self.spin = self.rng.choice([-1,1], (self.size, self.size))
 
+    # computes the energy difference from flipping the S_ij spin.
+    # Periodic boundary conditions
+    def energy_diff(self, i, j):
+        top = self.spin[(i-1)%self.size, j]
+        bottom = self.spin[(i+1)%self.size, j]
+        left = self.spin[i, (j-1)%self.size]
+        right = self.spin[i, (j+1)%self.size]
+
+        return 2*self.spin[i, j]*(top+bottom+left+right)
+
+    # does one attempt at flipping a spin
     def step(self):
-        i = random.randint(0,self.size-1)
-        j = random.randint(0,self.size-1)
+        i, j = self.rng.integers(0,self.size,2)
 
         deltaE = self.energy_diff(i,j)
         if deltaE <= 0:
-            self.spin[i][j] *= -1
+            self.spin[i, j] *= -1
         else:
-            flip_probability = math.exp(-deltaE/self.T)
-            if random.random() < flip_probability:
-                self.spin[i][j] *= -1
+            flip_probability = np.exp(-deltaE/self.T)
+            if self.rng.random() < flip_probability:
+                self.spin[i, j] *= -1
 
-    def energy_diff(self, i, j):
-        top = self.spin[(i-1)%self.size][j]
-        bottom = self.spin[(i+1)%self.size][j]
-        left = self.spin[i][(j-1)%self.size]
-        right = self.spin[i][(j+1)%self.size]
+    # does gridsize^2 attempts at flipping a spin
+    def MC_step(self):
+        for _ in range(self.size**2):
+            lattice.step()
 
-        return 2*self.spin[i][j]*(top+bottom+left+right)
 
 if __name__ == "__main__":
     gridsize = 100
     temperature = 1.5
     lattice = Lattice(gridsize, temperature)
     MC_steps = 100
-    for _ in range(MC_steps*gridsize**2):
-        lattice.step()
+    for _ in range(MC_steps):
+        lattice.MC_step()
     plt.imshow(lattice.spin, cmap="gray")
     plt.show()
