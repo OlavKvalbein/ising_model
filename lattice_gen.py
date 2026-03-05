8269e22225fcc0a236727cef97e8e80c891a7264
alextry004 <alextry004@gmail.com>
2026-03-05 15:39:31 +0100

    bugfixed some simulation results that did not match between the 2 plots

--- lattice_gen.py
+++ lattice_gen.py
@@ -14,41 +14,44 @@ T_values = [1.0, 1.5, 2.0, 2.5, 3.0]
 T_values = [abs(T) for T in T_values]
 MC_steps = 100
 
-
-for temperature in T_values:
-    lattice_n = Lattice(gridsize, temperature)
-    m_hist = []
-
-    for step in range(MC_steps):
-        for _ in range(gridsize**2):
-            lattice_n.step()
-        m_hist.append(spin_average(lattice_n.spin))
-
-    plt.plot(m_hist, label=f"T={temperature}")
-
-plt.xlabel("MC step")
-plt.ylabel("Spin average  m = <s>")
-plt.legend()
-plt.show()
+m_hists = {}
 
 
 fig, axes = plt.subplots(1, len(T_values), figsize=(8, 5))
 
 for ax, T in zip(axes, T_values):
     lattice_n = Lattice(gridsize, T)
+    m_hist = []
 
     for i in range(MC_steps):
-        print(f"\rGenerating lattice series... MC step {i+1}/{MC_steps}", end="", flush=True)
+        print(f"\rT={T} | Generating lattice series... MC step {i+1}/{MC_steps}", end="", flush=True)
 
         for _ in range(gridsize**2):
             lattice_n.step()
+
+        m_hist.append(abs(spin_average(lattice_n.spin)))
+
     print()
+    m_hists[T] = m_hist  # save for the second plot
+
     ax.imshow(lattice_n.spin, cmap="gray")
     ax.axis("off")
     ax.set_title(f"T = {T}")
-    
-    
 
 plt.tight_layout()
 plt.show()
+
+plt.figure()
+for T in T_values:
+    plt.plot(m_hists[T], label=f"T={T}")
+
+plt.xlabel("MC step")
+plt.ylabel("Spin average  |m| = |<s>|")
+plt.legend()
+plt.savefig("fig_1.png" , dpi = 200)
+plt.show()
+
+
+
+
 
