8894b3227bddeaae5571180459175862a95f191e
alextry004 <alextry004@gmail.com>
2026-03-19 15:51:26 +0100

    some changes

--- lattice_gen.py
+++ lattice_gen.py
@@ -3,15 +3,12 @@ from lattice import Lattice
 
 def spin_average(spin_2d):
     total = 0
-    n = 0
     for row in spin_2d:
         total += sum(row)
-        n += len(row)
-    return total / n
+    return total / gridsize**2
 
-gridsize = 64
+gridsize = 32
 T_values = [1.0, 1.5, 2.0, 2.5, 3.0]
-T_values = [abs(T) for T in T_values]
 MC_steps = 1000
 
 N_sims = 10
@@ -33,7 +30,7 @@ for T in T_values:
 
             lattice_n.MC_step()
 
-            m_sum[step] += abs(spin_average(lattice_n.spin))
+            m_sum[step] += spin_average(lattice_n.spin)
         print()
 
     m_hists[T] = [x / N_sims for x in m_sum]
